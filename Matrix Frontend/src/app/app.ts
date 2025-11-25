c4692cfbfffa5a3bc27ec0db9cdb2697614cfa2e
Zuhair Jarrar <es.zuhair.qussi2023@alexu.edu.eg>
2025-11-25 20:52:31 +0200

    fix scroll

--- Matrix Frontend/src/app/app.ts
+++ Matrix Frontend/src/app/app.ts
@@ -9,6 +9,8 @@ interface MatrixStep {
   currentMatrixB : number[];
   pivotIndex?: { r: number, c: number }; 
   highlightRow?: number;
+  L?:number[][];
+  U?:number[][];
 }
 
 interface SolutionResponse {
@@ -128,51 +130,136 @@ export class App {
       executionTime: 5 ,
       totalIterations : 6,
       equations:["x1 = x2/3","x2 = x3/3","x3 = x1/x2"],
-      steps: [
+      steps: [{
+        stepNumber: 1,
+        description: "diagonally dominant -> True",
+        currentMatrixA: [
+          [4, 2, 2],
+          [2, 5, 3],
+          [2, 3, 6]
+        ],
+        currentMatrixB: [8, 10, 11],
+      },
         {
-            stepNumber: 1,
-            description: "select pivot",
-            currentMatrixA: [[2,1,4],[1,2,3],[4,-1,2]],
-            currentMatrixB : [1,1.5,2],
-            pivotIndex: { r: 0, c: 0 },
-          },
-        {
-            stepNumber: 2,
-            description: "-0.5*r1+r2 ===> r2",
-            currentMatrixA: [[2,1,4],[0,1.5,1],[4,-1,2]],
-            currentMatrixB : [1,1,2],
-            pivotIndex: { r: 0, c: 0 },
-            highlightRow : 1
+          stepNumber: 2,
+          description: "Iter 1: Vector=[2.0000, 1.2000, 0.5667] | Error=1.0000",
+          currentMatrixA: [
+            [4, 2, 2],
+            [2, 5, 3],
+            [2, 3, 6]
+          ],
+          currentMatrixB: [2.0000000000, 1.2000000000, 0.5666666667],
         },
         {
-            stepNumber: 3,
-            description: "-2*r1+r3 ===> r3",
-            currentMatrixA: [[2,1,4],[0,1.5,1],[0,-3,-6]],
-            currentMatrixB : [1,1,0],
-            pivotIndex: { r: 0, c: 0 },
-            highlightRow : 2
+          stepNumber: 3,
+          description: "Iter 2: Vector=[1.1167, 1.2133, 0.8544] | Error=0.7910",
+          currentMatrixA: [
+            [4, 2, 2],
+            [2, 5, 3],
+            [2, 3, 6]
+          ],
+          currentMatrixB: [1.1166666670, 1.2133333330, 0.8544444445],
         },
         {
-            stepNumber: 4,
-            description: "select pivot",
-            currentMatrixA: [[2,1,4],[0,1.5,1],[0,-3,-6]],
-            currentMatrixB : [1,1,0],
-            pivotIndex: { r: 1, c: 1 },
+          stepNumber: 4,
+          description: "Iter 3: Vector=[0.9661, 1.1009, 0.9609] | Error=0.1558",
+          currentMatrixA: [
+            [4, 2, 2],
+            [2, 5, 3],
+            [2, 3, 6]
+          ],
+          currentMatrixB: [0.9661111112, 1.1008888890, 0.9608518518],
         },
         {
-            stepNumber: 5,
-            description: "2*r2+r3 ===> r3",
-            currentMatrixA: [[2,1,4],[0,1.5,1],[0,0,-4]],
-            currentMatrixB : [1,1,2],
-            pivotIndex: { r: 1, c: 1 },
-            highlightRow : 2
+          stepNumber: 5,
+          description: "Iter 4: Vector=[0.9691, 1.0358, 0.9924] | Error=0.0628",
+          currentMatrixA: [
+            [4, 2, 2],
+            [2, 5, 3],
+            [2, 3, 6]
+          ],
+          currentMatrixB: [0.9691296295, 1.0358370370, 0.9923716050],
         },
         {
-          stepNumber:6,
-          description:"x3 = 2/-4 = -0.5",
-          currentMatrixA:[[2,1,4],[0,1.5,1],[0,0,1]],
-          currentMatrixB : [1,1,-0.5],
+          stepNumber: 6,
+          description: "Iter 5: Vector=[0.9859, 1.0102, 0.9996] | Error=0.0254",
+          currentMatrixA: [
+            [4, 2, 2],
+            [2, 5, 3],
+            [2, 3, 6]
+          ],
+          currentMatrixB: [0.9858956790, 1.0102187650, 0.9995920578],
         },
+        {
+          stepNumber: 7,
+          description: "Iter 6: Vector=[0.9951, 1.0022, 1.0005] | Error=0.0092",
+          currentMatrixA: [
+            [4, 2, 2],
+            [2, 5, 3],
+            [2, 3, 6]
+          ],
+          currentMatrixB: [0.9950945885, 1.0022069300, 1.0005316720],
+        },
+        {
+          stepNumber: 8,
+          description: "Iter 7: Vector=[0.9986, 1.0002, 1.0003] | Error=0.0035",
+          currentMatrixA: [
+            [4, 2, 2],
+            [2, 5, 3],
+            [2, 3, 6]
+          ],
+          currentMatrixB: [0.9986306990, 1.0002287170, 1.0003420750],
+        },
+        {
+          stepNumber: 9,
+          description: "Iter 8: Vector=[0.9997, 0.9999, 1.0001] | Error=0.0011",
+          currentMatrixA: [
+            [4, 2, 2],
+            [2, 5, 3],
+            [2, 3, 6]
+          ],
+          currentMatrixB: [0.9997146040, 0.9999089134, 1.0001406750],
+        },
+        {
+          stepNumber: 10,
+          description: "Iter 9: Vector=[1.0000, 1.0000, 1.0000] | Error=0.0003",
+          currentMatrixA: [
+            [4, 2, 2],
+            [2, 5, 3],
+            [2, 3, 6]
+          ],
+          currentMatrixB: [0.9999752058, 0.9999255126, 1.0000455080],
+        },
+        {
+          stepNumber: 11,
+          description: "Iter 10: Vector=[1.0000, 1.0000, 1.0000] | Error=0.0000",
+          currentMatrixA: [
+            [4, 2, 2],
+            [2, 5, 3],
+            [2, 3, 6]
+          ],
+          currentMatrixB: [1.0000144900, 0.9999668992, 1.0000117200],
+        },
+        {
+          stepNumber: 12,
+          description: "Iter 11: Vector=[1.0000, 1.0000, 1.0000] | Error=0.0000",
+          currentMatrixA: [
+            [4, 2, 2],
+            [2, 5, 3],
+            [2, 3, 6]
+          ],
+          currentMatrixB: [1.0000106900, 0.9999886920, 1.0000020910],
+        },
+        {
+          stepNumber: 13,
+          description: "Iter 12: Vector=[1.0000, 1.0000, 1.0000] | Error=0.0000",
+          currentMatrixA: [
+            [4, 2, 2],
+            [2, 5, 3],
+            [2, 3, 6]
+          ],
+          currentMatrixB: [1.0000046080, 0.9999969022, 1.0000000130],
+        }
       ],
       errorMessage:null
     };
